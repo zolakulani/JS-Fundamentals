bd2d31410bc34e51c80d88b82e134de3472cb71b
zolakulani <shikwenizolakulani@gmaile.com>
2025-05-24 00:16:30 +0200

    console

---
+++ 0-javascript_is_amazing.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+// Write a script that prints “JavaScript is amazing”:
+
+// You must create a constant variable called myVar with the value “JavaScript is amazing”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+
+let command = parseInt(process.argv[2]);
+console.log('JavaScript is amazing');
